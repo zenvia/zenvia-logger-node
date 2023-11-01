1369e4e8df79ccdd9a1711299e0609bc0be09d62
rafaelgpimenta-zenvia <rafael.pimenta@zenvia.com>
2023-11-01 18:45:32 -0300

    simplify

--- src/middleware/trace.d.ts
+++ src/middleware/trace.d.ts
@@ -1,10 +1,9 @@
-import { IncomingMessage, ServerResponse } from 'http'
 import rTracer from 'cls-rtracer';
 
 declare const traceMiddleware: () =>
-  ((req: IncomingMessage, res: ServerResponse, next: (err?: any) => void) => void) |
-  ((fastify: any, options: rTracer.IFastifyOptions, done: (err?: any) => void) => void) |
-  (rTracer.IHapiPlugin<rTracer.IOptions>) |
-  ((ctx: { request: IncomingMessage; response: ServerResponse<IncomingMessage>; }, next: () => Promise<void>) => Promise<void>);
+  ReturnType<typeof rTracer.expressMiddleware> |
+  ReturnType<typeof rTracer.fastifyPlugin> |
+  typeof rTracer.hapiPlugin |
+  ReturnType<typeof rTracer.koaMiddleware>;
 
 export default traceMiddleware;
