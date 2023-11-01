c52232e191722f1fed7a91f492a93766d498e94c
rafaelgpimenta-zenvia <rafael.pimenta@zenvia.com>
2023-11-01 16:54:25 -0300

    fix types

--- src/middleware/trace.d.ts
+++ src/middleware/trace.d.ts
@@ -1,3 +1,10 @@
-declare const traceMiddleware: () => (req: IncomingMessage, res: ServerResponse, next: (err?: any) => void) => void;
+import { IncomingMessage, ServerResponse } from 'http'
+import rTracer from 'cls-rtracer';
+
+declare const traceMiddleware: () =>
+  ((req: IncomingMessage, res: ServerResponse, next: (err?: any) => void) => void) |
+  ((fastify: any, options: rTracer.IFastifyOptions, done: (err?: any) => void) => void) |
+  (rTracer.IHapiPlugin<rTracer.IOptions>) |
+  ((ctx: { request: IncomingMessage; response: ServerResponse<IncomingMessage>; }, next: () => Promise<void>) => Promise<void>);
 
 export default traceMiddleware;
